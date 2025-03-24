58f25a9afdb2aede3253b71ae4ef6d4f9cd98409
Angel Ancheta <angelancheta98@yahoo.com>
2025-03-24 16:05:39 -0700

    added interpretation and next steps

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+let total = 0;
+let interpretation = "";
+
+function interpret(total){
+    if (total<=10) {
+        interpretation=`Ego rarely influences your actions. This may reflect strong self-awareness or minimal ego interference. \nReflect on whether you avoid ego-driven behaviors or if they go unnoticed. Strengthen self-awareness.`
+    } else if (total > 10 && total <=20) {
+        interpretation=`Ego occasionally impacts your thoughts or behaviors \nIdentify triggers for ego-driven actions and explore strategies to manage them.`
+    }else if (total > 20 && total <=32) {
+        interpretation=`Ego plays a significant role in shaping your responses and interactions. \nFocus on building humility, reducing comparison, and strengthening intrinsic motivations.`
+    }
+    document.getElementById("interpretation").innerHTML += interpretation
+}
 function calculate(event) {
     event.preventDefault();
 
@@ -8,7 +21,7 @@ function calculate(event) {
         nedb: ['nedb1', 'nedb2']
     };
 
-    let total = 0;
+    total = 0;
 
     for (const [category, ids] of Object.entries(categories)) {
         const sum = ids.reduce((acc, id) => acc + parseInt(document.getElementById(id).value), 0);
@@ -22,6 +35,7 @@ function calculate(event) {
     document.getElementById('total').textContent = `Total: ${total}`;
 
     document.querySelector('.btn').disabled = true;
+    interpret(total)
 }
 
 document.addEventListener('DOMContentLoaded', function() {
